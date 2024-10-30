2d25005c590403fc657c541aae1fcfd8d2c4d01a
faycalEdderi <faycmx@live.fr>
2024-10-30 21:57:34 +0100

    correction on apiUrl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-axios.get(`${apiUrl}/your-endpoint`)
+axios.get(`${apiUrl}`)
   .then(response => {
     // gérer la réponse ici
   })
