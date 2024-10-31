02f54a9a57998a5c11bf47c5181976a1419f0126
faycalEdderi <faycmx@live.fr>
2024-10-31 10:33:20 +0100

    make frontend and backend great again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import axios from 'axios';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-axios.get(`${apiUrl}`)
-  .then(response => {
-    // gérer la réponse ici
-  })
-  .catch(error => {
-    console.error('Erreur API:', error);
-  });
+const App = () => {
+  React.useEffect(() => {
+    axios.get(`${apiUrl}`)
+      .then(response => {
+        // Gérer la réponse ici
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Erreur API:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Mon Application</h1>
+    </div>
+  );
+};
+
+export default App;
